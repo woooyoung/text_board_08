89c16e8b55c35ea0f40742da227320641245a81d
woooyoung <axdsw121@gmail.com>
2021-08-23 12:08:55 +0900

    article list 명령시에 데이터를 DB에서 가져오기

--- src/com/sbs/example/textBoard/Main.java
+++ src/com/sbs/example/textBoard/Main.java
@@ -3,6 +3,7 @@ package com.sbs.example.textBoard;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,8 +12,6 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
-		List<Article> articles = new ArrayList<>();
 
 		int lastArticleId = 0;
 
@@ -30,9 +29,6 @@ public class Main {
 				title = sc.nextLine();
 				System.out.printf("내용 : ");
 				body = sc.nextLine();
-
-				Article article = new Article(id, title, body);
-				articles.add(article);
 
 				Connection conn = null;
 				PreparedStatement pstat = null;
@@ -78,10 +74,69 @@ public class Main {
 
 				lastArticleId++;
 
-				System.out.println(article);
-
 			} else if (command.equals("article list")) {
 				System.out.println("== 게시글 리스트 ==");
+
+				List<Article> articles = new ArrayList<>();
+
+				Connection conn = null;
+				PreparedStatement pstat = null;
+				ResultSet rs = null;
+
+				try {
+					Class.forName("com.mysql.jdbc.Driver");
+					String url = "jdbc:mysql://127.0.0.1:3306/text_board?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+
+					conn = DriverManager.getConnection(url, "root", "");
+					System.out.println("연결 성공!");
+
+					String sql = "SELECT *";
+					sql += " FROM article";
+					sql += " ORDER BY id DESC;";
+
+//					System.out.println(sql);
+
+					pstat = conn.prepareStatement(sql);
+					rs = pstat.executeQuery(sql);
+
+					while (rs.next()) {
+						int id = rs.getInt("id");
+						String regDate = rs.getString("regDate");
+						String updateDate = rs.getString("updateDate");
+						String title = rs.getString("title");
+						String body = rs.getString("body");
+
+						Article article = new Article(id, regDate, updateDate, title, body);
+						articles.add(article);
+					}
+
+				} catch (ClassNotFoundException e) {
+					System.out.println("드라이버 로딩 실패");
+				} catch (SQLException e) {
+					System.out.println("에러: " + e);
+				} finally {
+					try {
+						if (rs != null && !rs.isClosed()) {
+							rs.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+					try {
+						if (pstat != null && !pstat.isClosed()) {
+							pstat.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+					try {
+						if (conn != null && !conn.isClosed()) {
+							conn.close();
+						}
+					} catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
 
 				if (articles.size() == 0) {
 					System.out.println("게시물이 존재하지 않습니다.");
