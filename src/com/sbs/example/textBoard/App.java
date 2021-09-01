0dd554581504f820250a1e9d22e92666405d2de8
woooyoung <axdsw121@gmail.com>
2021-09-01 11:51:52 +0900

    article 컨트롤러 도입

--- src/com/sbs/example/textBoard/App.java
+++ src/com/sbs/example/textBoard/App.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
+import com.sbs.example.textBoard.controller.ArticleController;
 import com.sbs.example.textBoard.controller.MemberController;
 import com.sbs.example.textBoard.util.DBUtil;
 import com.sbs.example.textBoard.util.SecSql;
@@ -65,132 +66,23 @@ public class App {
 		memberController.setConn(conn);
 		memberController.setScanner(sc);
 
+		ArticleController articleController = new ArticleController();
+		articleController.setConn(conn);
+		articleController.setScanner(sc);
+
 		if (command.equals("member join")) {
 			memberController.join(command);
-
 		} else if (command.equals("article write")) {
-			String title;
-			String body;
-
-			System.out.println("== 게시글 생성 ==");
-			System.out.printf("제목 : ");
-			title = sc.nextLine();
-			System.out.printf("내용 : ");
-			body = sc.nextLine();
-
-			SecSql sql = new SecSql();
-
-			sql.append("INSERT INTO article");
-			sql.append("SET regDate = NOW()");
-			sql.append(", updateDate = NOW()");
-			sql.append(", title = ?", title);
-			sql.append(", `body` = ?", body);
-
-			int id = DBUtil.insert(conn, sql);
-
-			System.out.printf("%d번 게시물이 생성되었습니다.\n", id);
-
+			articleController.write(command);
 		} else if (command.startsWith("article delete ")) {
-			int id = Integer.parseInt(command.split(" ")[2]);
-
-			System.out.printf("== %d번 게시글 삭제 ==\n", id);
-
-			SecSql sql = new SecSql();
-			sql.append("SELECT COUNT(*) AS cnt");
-			sql.append("FROM article");
-			sql.append("WHERE id = ?", id);
-			int articlesCount = DBUtil.selectRowIntValue(conn, sql);
-
-			if (articlesCount == 0) {
-				System.out.printf("%d번 게시글은 존재하지 않습니다.\n", id);
-				return 0;
-			}
-
-			sql = new SecSql();
-			sql.append("DELETE FROM article");
-			sql.append("WHERE id = ?", id);
-
-			DBUtil.delete(conn, sql);
-
-			System.out.printf("%d번 게시글이 삭제되었습니다.\n", id);
-
+			articleController.delete(command);
 		} else if (command.startsWith("article detail ")) {
-			int id = Integer.parseInt(command.split(" ")[2]);
-
-			System.out.printf("== %d번 게시글 상세보기 ==\n", id);
-
-			SecSql sql = new SecSql();
-			sql.append("SELECT *");
-			sql.append("FROM article");
-			sql.append("WHERE id = ?", id);
-			Map<String, Object> articleMap = DBUtil.selectRow(conn, sql);
-
-			if (articleMap.isEmpty()) {
-				System.out.printf("%d번 게시글은 존재하지 않습니다.\n", id);
-				return 0;
-			}
-
-			Article article = new Article(articleMap);
-
-			System.out.printf("번호 : %d\n", article.id);
-			System.out.printf("작성날짜 : %s\n", article.regDate);
-			System.out.printf("수정날짜 : %s\n", article.updateDate);
-			System.out.printf("제목 : %s\n", article.title);
-			System.out.printf("내용 : %s\n", article.body);
-
+			articleController.showDetail(command);
 		} else if (command.startsWith("article modify ")) {
-			int id = Integer.parseInt(command.split(" ")[2]);
-			String title;
-			String body;
-
-			System.out.printf("== %d번 게시글 수정 ==\n", id);
-			System.out.printf("새 제목 : ");
-			title = sc.nextLine();
-			System.out.printf("새 내용 : ");
-			body = sc.nextLine();
-
-			SecSql sql = new SecSql();
-
-			sql.append("UPDATE article");
-			sql.append("SET updateDate = NOW()");
-			sql.append(", title = ?", title);
-			sql.append(", `body` = ?", body);
-			sql.append("WHERE id = ?", id);
-
-			DBUtil.update(conn, sql);
-
-			System.out.printf("%d번 게시글이 수정되었습니다.\n", id);
-
+			articleController.modify(command);
 		} else if (command.equals("article list")) {
-			System.out.println("== 게시글 리스트 ==");
-
-			List<Article> articles = new ArrayList<>();
-
-			SecSql sql = new SecSql();
-
-			sql.append("SELECT *");
-			sql.append("FROM article");
-			sql.append("ORDER BY id DESC");
-
-			List<Map<String, Object>> articlesListMap = DBUtil.selectRows(conn, sql);
-
-			for (Map<String, Object> articleMap : articlesListMap) {
-				articles.add(new Article(articleMap));
-			}
-
-			if (articles.size() == 0) {
-				System.out.println("게시물이 존재하지 않습니다.");
-				return 0;
-			}
-			System.out.println("번호  /  제목");
-
-			for (Article article : articles) {
-				System.out.printf("%d    /   %s\n", article.id, article.title);
-			}
-
-		}
-
-		else if (command.equals("system exit")) {
+			articleController.showList(command);
+		} else if (command.equals("system exit")) {
 			System.out.println("== 프로그램 종료 ==");
 			return -1;
 		}
